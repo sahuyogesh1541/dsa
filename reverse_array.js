363a452db3210718bac71785e5ae13e09907673c
YogeshhSahu <yogeshhhhh7869@gmail.com>
2026-01-06 22:47:06 +0530

    reverse array through two pointer

--- reverse_array.js
+++ reverse_array.js
@@ -1,14 +1,13 @@
-let arr = [ 1,3,4,5,7]
+// Two Pointer
+let arr = [ 1,3,4,5,7] 
 
 let low = 0,high = arr.length -1;
-
-for(let i = 0; i < arr.length ; i++){
-    if(low < high){
-        let temp =  arr[i];
-        arr[i] = arr[high];
-        arr[high] = temp; 
-        low++ 
-        high --
-    }
-}
+ 
+while(low < high){
+    let temp =  arr[low];
+    arr[low] = arr[high];
+    arr[high] = temp; 
+    low++ 
+    high --
+} 
 console.log(arr)
