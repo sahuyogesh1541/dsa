91210e9b07f60e09b41c03737c9066cd89c1b380
YogeshhSahu <yogeshhhhh7869@gmail.com>
2026-01-06 23:52:40 +0530

    zeros at end most opimize way

--- zeros_at_end.js
+++ zeros_at_end.js
@@ -1,5 +1,19 @@
 let arr = [0,1,0,3,12] 
 
+// most optmized way
+let slow = 0 
+for(let i = 1 ; i< arr.length; i++){
+    if(arr[i] != 0){
+        let temp = arr[slow]
+        arr[slow]  = arr[i]
+        arr[i] = temp
+        slow++; 
+    } 
+} 
+console.log(arr)
+
+
+
 // one way using fil function
 // let temp  = []
 // for(let i in arr){
@@ -12,16 +26,17 @@ let arr = [0,1,0,3,12]
 // console.log(temp)
 
 // through two pointer
-let slow = 0 ,fast = 1
-for(let i = 0 ; i< arr.length -1; i++){
-    if(arr[slow] == 0  && arr[fast] != 0){
-        let temp = arr[slow]
-        arr[slow]  = arr[fast]
-        arr[fast] = temp
-        slow++;
-        fast++ 
-    } else if(arr[slow] == 0){
-        fast ++
-    } 
-} 
-console.log(arr)
+// let slow = 0 ,fast = 1
+// for(let i = 0 ; i< arr.length -1; i++){
+//     if(arr[slow] == 0  && arr[fast] != 0){
+//         let temp = arr[slow]
+//         arr[slow]  = arr[fast]
+//         arr[fast] = temp
+//         slow++;
+//         fast++ 
+//     } else if(arr[slow] == 0){
+//         fast ++
+//     } 
+// } 
+// console.log(arr)
+
