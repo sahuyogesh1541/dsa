49604a4f62b23a68a93554559f438e93b2eb1a33
YogeshhSahu <yogeshhhhh7869@gmail.com>
2026-01-07 23:21:32 +0530

    two sum using two pointer

--- two_sum.js
+++ two_sum.js
@@ -2,13 +2,22 @@ let arr = [1, 2, 3, 4, 6],target = 6
 
 let pair = []
 
+// using nested loop
+// for(let i= 0; i< arr.length; i++){
+//     for(let j = i+1;j< arr.length; j++){
+//         if(target - arr[j] == arr[i]){
+//             pair = [arr[i],arr[j]]
+//         }
+//     }
+// }
+// console.log(pair)
 
-for(let i= 0; i< arr.length; i++){
-    for(let j = i+1;j< arr.length; j++){
-        if(target - arr[j] == arr[i]){
-            pair = [arr[i],arr[j]]
-        }
-    }
+let next = arr.length-1
+for(let i = 0; i< arr.length; i++){
+     if(arr[i] + arr[next] == target){
+        pair = [arr[i], arr[next]]
+        break;
+     }
+     next--;
 }
 console.log(pair)
-
